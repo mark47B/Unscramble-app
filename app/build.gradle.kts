7c8acbfbfc56c168837966964881d1fdf75eb298
mark47B <windachae47@gmail.com>
2024-03-04 07:25:00 +0300

    Add Unit tests: CorrectWordGuessed, IncorrectGuess, Initialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,9 +74,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    testImplementation("junit:junit:4.13.2")
 }
